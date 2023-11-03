b58ef27090b6acba6c5c825bd5ac679e13fe3c2a
damnitkush <kbrheking.360@gmail.com>
2023-11-04 00:48:07 +0530

    kushagra

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/pages/Root";
 import ErrorPage from "./ErrorPage";
 import Theme from "./context/darkModeContext";
 import { useState } from "react";
+import Home from "./routes/root/Home";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -14,6 +14,7 @@ function App() {
       path: "/",
       element: <Root />,
       errorElement: <ErrorPage />,
+      children:[{path:"",element:<Home/>}]
     },
   ]);
   return (
