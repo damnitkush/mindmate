70ed3b3215471407d2465abf0a9b853f700246a6
damnitkush <kbrheking.360@gmail.com>
2023-11-03 20:50:52 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-<<<<<<< HEAD
-=======
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/pages/Root";
 import ErrorPage from "./ErrorPage";
 
->>>>>>> 917e024aa58138db3336fdce475ed07d20b0ee5d
+
 function App() {
   const router = createBrowserRouter([
     {
