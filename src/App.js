7a5515b9a2ff11a6485642335c35d18eaa71678d
damnitkush <kbrheking.360@gmail.com>
2023-11-03 20:41:41 +0530

    gun

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello2</div>;
+  return <div className="App">Hello666</div>;
 }
 
 export default App;
