917e024aa58138db3336fdce475ed07d20b0ee5d
sakshxm08 <saksham2211gambhir@gmail.com>
2023-11-03 20:46:22 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/pages/Root";
+import ErrorPage from "./ErrorPage";
 
 function App() {
-  return <div className="App">Hello666</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/assets/assets.js
@@ -0,0 +1,4 @@
+import logoColored from "./logo-colored.png";
+import logoTransparent from "./logo-transparent.png";
+
+export { logoColored, logoTransparent };
